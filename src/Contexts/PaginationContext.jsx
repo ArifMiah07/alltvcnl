b226bbd8881b85480ef48181e69f1d822acecf47
ArifMiah07 <arifmiah.me322@gmail.com>
2025-12-27 18:01:09 +0600

    testing pagination

--- src/Contexts/PaginationContext.jsx
+++ src/Contexts/PaginationContext.jsx
@@ -6,12 +6,25 @@ export const PaginationContext = createContext();
 export const PaginationProvider = ({ children }) => {
   const [currentPage, setCurrentPage] = useState(10);
   const [itemsPerPage, setItemsPerPage] = useState(10);
+  const [totalItems, setTotalItems] = useState(0);
+  const [totalPage, setTotalPage] = useState(1);
+
+  const [currentPageInputRange, setCurrentPageInputRange] =
+    useState(currentPage);
+  // const [channelsPerPage, setChannelsPerPage] = useState(10);
+  // const [channelsInput, setChannelsInput] = useState(10);
 
   const getValues = {
     currentPage,
     setCurrentPage,
     itemsPerPage,
     setItemsPerPage,
+    totalItems,
+    setTotalItems,
+    totalPage,
+    setTotalPage,
+    currentPageInputRange,
+    setCurrentPageInputRange,
   };
   return (
     <PaginationContext.Provider value={getValues}>
--- src/Pages/iptv-player/streams/Streams.jsx
+++ src/Pages/iptv-player/streams/Streams.jsx
@@ -23,8 +23,18 @@ const Streams = () => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   //
-  const { currentPage, setCurrentPage, itemsPerPage, setItemsPerPage } =
-    usePagination();
+  const {
+    currentPage,
+    setCurrentPage,
+    itemsPerPage,
+    setItemsPerPage,
+    totalItems,
+    setTotalItems,
+    totalPage,
+    setTotalPage,
+    currentPageInputRange,
+    setCurrentPageInputRange,
+  } = usePagination();
 
   // effects
   useEffect(() => {
@@ -52,33 +62,56 @@ const Streams = () => {
 
   console.log(streams);
 
+  const handleCurrentPage = (page) => {
+    setCurrentPage(Number(page));
+  };
+
+  //
+  const startPage = Math.max(1, currentPage - 4);
+  const pagesArray = Array.from({ length: 10 }, (_, i) => startPage + i);
+
   //
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error}</p>;
 
-  //
+  console.log(pagesArray);
   return (
     <div className="min-h-screen w-full p-4">
       <h1>this is </h1>
       <p>total pages {streams?.length}</p>
       <div>Streams Page</div>
       <div></div>
-      <div className="w-full p-3 grid grid-cols-2 gap-5">
-        {streams.map((stream, index) => (
+      <div className="w-full p-3 grid grid-cols-3 gap-5">
+        {streams.map((stream_item, index) => (
           <div className="w-full border border-red-500 p-3" key={index}>
             <p>{index}</p>
             <div className="border border-blue-500 w-full h-full">
               <ReactPlayer
-                controls
-                src={stream?.url}
-                width={500}
-                height={300}
+                // pip={true}
+                controls={true}
+                src={stream_item.url}
+                width="100%"
+                height="100%"
               />
             </div>
           </div>
         ))}
       </div>
-      <div></div>
+      {/* pagination */}
+      <div className="w-full border border-red-400">
+        <div className="border border-red-600 p-2 flex flex-row justify-between items-center">
+          {pagesArray?.map((page, index) => (
+            <button
+              onClick={() => handleCurrentPage(page)}
+              key={index}
+              className={` border border-[#ff00ff] text-md rounded-sm hover:bg-[#a100ff] hover:text-white  py-2 px-5  ${
+                page === currentPage ? "bg-green-500 text-white" : ""
+              }  `}>
+              {page}
+            </button>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
