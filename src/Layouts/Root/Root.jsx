aa66345700906f20101452e3404b21e30fca2a91
ArifMiah07 <arifmiah.me322@gmail.com>
2026-01-02 18:02:47 +0600

    add a radial gradient background

--- src/Layouts/Root/Root.jsx
+++ src/Layouts/Root/Root.jsx
@@ -26,7 +26,7 @@ const Root = () => {
 
       </div> */}
       <div
-        className="absolute z-4 w-full h-full -top-[360px] left-1/2 -translate-x-1/2 rounded-full bg-[radial-gradient(circle_at_30%_30%,_#A100FF_0%,_#3E1899_45%,_transparent_60%),_radial-gradient(circle_at_70%_35%,_#FF00B2_0%,_#99188C_45%,_transparent_60%),_radial-gradient(circle_at_50%_70%,_#00FFCC_0%,_#189984_45%,_transparent_60%)] blur-[500px] opacity-30 pointer-events-none
+        className="absolute z-4 w-full h-full -top-[360px] left-1/2 -translate-x-1/2 rounded-full bg-[radial-gradient(circle_at_30%_30%,_#A100FF_0%,_#3E1899_45%,_transparent_60%),_radial-gradient(circle_at_70%_35%,_#FF00B2_0%,_#99188C_45%,_transparent_60%),_radial-gradient(circle_at_50%_70%,_#00FFCC_0%,_#189984_45%,_transparent_60%)] blur-[500px] opacity-20 pointer-events-none
   "
       />
 
