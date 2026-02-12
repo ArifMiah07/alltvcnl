9ea6dcff8b6fb4b7692b88c2f84ef5d76248ad3d
ArifMiah07 <arifmiah.me322@gmail.com>
2026-02-12 16:39:48 +0600

    update

--- src/Pages/iptv-player/search/SearchStreams.jsx
+++ src/Pages/iptv-player/search/SearchStreams.jsx
@@ -169,7 +169,6 @@ const SearchStreams = () => {
     { length: endPage - startPage + 1 },
     (_, i) => startPage + i,
   );
-  //   console.log(pagesArray);
   // ____UPDATED CODE FROM CHATGPT____ //
   /** ______ENDs HERE______ */
 
