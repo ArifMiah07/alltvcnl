b86589a0eb3569fe02b6621ff70f377d9445e881
ArifMiah07 <arifmiah.me322@gmail.com>
2026-02-20 17:23:48 +0600

    working on favorites page

--- src/Pages/iptv-player/favorites/Favorites.jsx
+++ src/Pages/iptv-player/favorites/Favorites.jsx
@@ -2,14 +2,38 @@ import { useState } from "react";
 import HlsVideoPlayer from "../../../Components/hls-video-player/HlsVideoPlayer";
 import { useLocalStorage } from "../../../hooks/useLocalStorage";
 
+import PropTypes from "prop-types";
+import {
+  Bookmark,
+  BookmarkCheck,
+  Fullscreen,
+  MonitorPlay,
+  ListPlus,
+} from "lucide-react";
+// import useFetchStreams from "../../../hooks/useFetch";
+// import { usePagination } from "../../../hooks/usePagination";
+import { Link } from "react-router-dom";
+
 const Favorites = () => {
-  const { bookmarkedChannel } = useLocalStorage();
-  const allBookmarkedChannels = Object.keys(bookmarkedChannel);
+  const { bookmarkedChannel, handleBookmarkChannelToggle } = useLocalStorage();
+  const allBookmarkedChannels = Object.entries(bookmarkedChannel);
   const [currentPageNumber, setCurrentPageNumber] = useState(1);
   const [channelsPerPage, setChannelsPerPage] = useState(10);
 
   //
   const [activeTab, setActiveTab] = useState("list");
+
+  // const { streams } = useFetchStreams();
+  // const {
+  //   currentPage,
+  //   // channelsPerPage,
+  // } = usePagination();
+
+  // react states
+  const [specificChannelStream, setSpecificChannelStream] = useState({});
+  const handleSpecificChannelStream = (channelInfo) => {
+    setSpecificChannelStream(channelInfo);
+  };
 
   // console.log(getStreams);// getStreams.data => 12k+
   const totalItems = allBookmarkedChannels.length || 0;
@@ -99,9 +123,19 @@ const Favorites = () => {
           handleCurrentPage={handleCurrentPage}
           currentPageNumber={currentPageNumber}
           numbersOfPages={numbersOfPages}
+          channelsPerPage={channelsPerPage}
+          handleSpecificChannelStream={handleSpecificChannelStream}
+          handleBookmarkChannelToggle={handleBookmarkChannelToggle}
+          bookmarkedChannel={bookmarkedChannel}
         />
       )}
       {activeTab === "table" && <Table />}
+
+      {Object.entries(bookmarkedChannel).forEach(([key, value]) => {
+        console.log("URL:", key);
+        console.log("Channel:", value.channel);
+        console.log("Quality:", value.quality);
+      })}
     </section>
   );
 };
@@ -122,6 +156,10 @@ function Grid({
   handleCurrentPage,
   currentPageNumber,
   numbersOfPages,
+  channelsPerPage,
+  handleSpecificChannelStream,
+  handleBookmarkChannelToggle,
+  bookmarkedChannel,
 }) {
   return (
     <div>
@@ -130,12 +168,91 @@ function Grid({
         {/* contents */}
         <div className="w-full h-full lg:col-span-9 ">
           <div className=" border w-full h-full  p-2 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-start justify-evenly ">
-            {paginatedStreams?.map((channel, index) => (
-              <div className="p-2" key={index}>
-                <HlsVideoPlayer src={channel} />
-                {/* <div>s */}
-              </div>
-            ))}
+            <div className=" w-full h-full col-span-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 items-center justify-items-center gap-4 p-4  ">
+              {paginatedStreams ? (
+                paginatedStreams?.map(([url, stream], index) => (
+                  // player container
+                  <div
+                    className=" w-full h-full flex flex-col items-center justify-center border border-rose-50 bg-radial-[at_50%_75%] from-sky-100 via-violet-100 to-fuchsia-100 to-90%"
+                    key={url}>
+                    {/* user actions */}
+                    <div className="w-full flex flex-col flex-wrap">
+                      {/* channel info */}
+                      <div className="flex gap-2 px-2 text-[18px]">
+                        {/* channel number */}
+                        <span className="font-medium dark:text-white">
+                          {(currentPageNumber - 1) * channelsPerPage +
+                            (index + 1)}
+                          .
+                        </span>
+                        {/* channel name or title */}
+                        <p className="dark:text-white">
+                          {stream?.channel ? stream.channel : stream.title}
+                        </p>
+                      </div>
+                      {/* basic actions */}
+                      <div className="w-full flex flex-row gap-2 flex-wrap items-center p-2  ">
+                        {/* stream a specific channel */}
+                        <span
+                          onClick={() =>
+                            handleSpecificChannelStream({ stream })
+                          }
+                          className=" flex flex-col items-center justify-center rounded-sm bg-purple-200 hover:bg-purple-300  w-6 h-6 ">
+                          <Link
+                            to={`/specific-channel/${
+                              (currentPageNumber - 1) * channelsPerPage +
+                              (index + 1)
+                            }/${encodeURIComponent(
+                              stream.channel || stream.title,
+                            )}`}
+                            state={{ streamData: stream }}>
+                            <Fullscreen className="" />
+                          </Link>
+                        </span>
+                        {/* stream a specific channel on browser in a separate tab */}
+                        <span className=" flex flex-col items-center justify-center rounded-sm bg-purple-200 hover:bg-purple-300  w-6 h-6 ">
+                          <a
+                            href={`${stream.url}`}
+                            target="_blank"
+                            rel="noopener noreferrer">
+                            <MonitorPlay />
+                          </a>
+                        </span>
+                        {/* bookmark a specific channel */}
+                        {/* save or locally or save to a playlist <localStorage || default, playlist name> */}
+                        <span
+                          onClick={() => handleBookmarkChannelToggle(stream)}
+                          className=" flex flex-col items-center justify-center rounded-sm bg-purple-200 hover:bg-purple-300  w-6 h-6 ">
+                          {bookmarkedChannel[stream.url] ? (
+                            <BookmarkCheck />
+                          ) : (
+                            <Bookmark />
+                          )}
+                        </span>
+                        <span className=" flex flex-col items-center justify-center rounded-sm bg-purple-200 hover:bg-purple-300  w-6 h-6 ">
+                          <ListPlus />
+                        </span>
+                        {(stream.feed || stream.quality) && (
+                          <div className="flex flex-row gap-3 dark:text-white ">
+                            {stream.feed && <p>{stream.feed}</p>}
+                            {stream.quality && <p>{stream.quality}</p>}
+                          </div>
+                        )}
+                      </div>
+                    </div>
+                    <div className="w-full h-full flex flex-col border border-green-50  ">
+                      <HlsVideoPlayer
+                        src={stream?.url}
+                        controls
+                        autoPlay={false}
+                      />
+                    </div>
+                  </div>
+                ))
+              ) : (
+                <p className="dark:text-white">No streams to play</p>
+              )}
+            </div>
           </div>
         </div>
         {/* pagination */}
@@ -171,6 +288,18 @@ function Grid({
     </div>
   );
 }
+
+Grid.propTypes = {
+  paginatedStreams: PropTypes.array,
+  pagesArray: PropTypes.array,
+  handleCurrentPage: PropTypes.func,
+  currentPageNumber: PropTypes.number || PropTypes.string,
+  numbersOfPages: PropTypes.number || PropTypes.string,
+  channelsPerPage: PropTypes.number || PropTypes.string,
+  handleSpecificChannelStream: PropTypes.func,
+  handleBookmarkChannelToggle: PropTypes.func,
+  bookmarkedChannel: PropTypes.object,
+};
 
 function Table() {
   return (
