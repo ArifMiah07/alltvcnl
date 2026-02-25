bd840999e3e7ccb6a52a436f7adef25e1763426b
ArifMiah07 <arifmiah.me322@gmail.com>
2026-02-25 18:47:03 +0600

    working on adda page

--- src/Pages/adda/Adda.jsx
+++ src/Pages/adda/Adda.jsx
@@ -2,40 +2,47 @@
 
 const Adda = () => {
   return (
-    <section>
+    <section className="w-full min-h-screen p-4">
       <header>
         <h1>Gossip here with people from world wide</h1>
       </header>
       <main>
         <section>All posts</section>
-        <section>
+        <section className=" w-full h-full grid grid-cols-12">
+          {/*  */}
           {/* interaction */}
-          <div>
-            <div>index</div>
-            <div>reactions</div>
-            <div>comment</div>
-            <div>share</div>
+          <div className=" w-full flex flex-col col-span-1">
+            <div className="bg-[#FEAEAE]">index</div>
+            <div className="bg-[#FFF3A2]">reactions</div>
+            <div className="bg-[#E9FFC7]">comment</div>
+            <div className="bg-[#B1D4FF]">share</div>
           </div>
           {/* content */}
-          <div>
-            <div>
-              {/* heading and caption */}
-              <div>
-                <div>
-                  <h1>heading</h1>
-                </div>
-                <div>
-                  <p>captions</p>
+          <div className="  w-full flex flex-col col-span-10">
+            <div className="grid grid-rows-4">
+              <div className=" row-span-3 w-full flex flex-row ">
+                {/* heading and caption */}
+                <div className="w-full grid grid-cols-12">
+                  <div className=" col-span-8 bg-[#979EC6]  w-full flex flex-col ">
+                    <div>
+                      <h1>heading</h1>
+                    </div>
+                    <div>
+                      <p>captions</p>
+                    </div>
+                  </div>
+                  <div className=" col-span-4 bg-[#D9D9D9] ">
+                    <p>files (images, video, audio, docs)</p>
+                  </div>
                 </div>
               </div>
-              <div>files</div>
-            </div>
-            <div>
-              <p>timestamps</p>
+              <div className="bg-[#B1D4FF] row-span-1">
+                <p>timestamps</p>
+              </div>
             </div>
           </div>
           {/* user choice */}
-          <div>
+          <div className="bg-[#D999FF] w-full flex flex-col  col-span-1">
             <div>setting</div>
             <div>expand</div>
             <div>full view</div>
