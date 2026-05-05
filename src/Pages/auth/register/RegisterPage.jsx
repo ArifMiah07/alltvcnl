cc90935e681cd90e6b704f81a5e1a21cbc56823a
ArifMiah07 <arifmiah.me322@gmail.com>
2026-05-06 02:03:21 +0600

    designed and created register page

--- src/Pages/auth/register/RegisterPage.jsx
+++ src/Pages/auth/register/RegisterPage.jsx
@@ -1,10 +1,11 @@
+import { Link } from "react-router-dom";
 import google_img from "../../../assets/images/google.png";
 
 const RegisterPage = () => {
   return (
-    <div className=" w-full min-h-screen z-10 p-4">
+    <div className=" w-full min-h-screen flex flex-col items-center justify-center z-10 p-4 auth_page_background">
       {/* <h1 className=""></h1> */}
-      <section className="lg:w-1/3 border border-black bg-">
+      <section className="lg:w-1/3 bg-white border border-black bg-">
         <h1 className=" text-3xl font-black text-black text-center my-12  ">
           Sign Up and
           <br /> Play Free
@@ -13,30 +14,31 @@ const RegisterPage = () => {
           <div className="flex flex-col items-center gap-3   rounded-full   p-1">
             {/* <label htmlFor="username">User Name</label> */}
             <input
-              className="w-[90%] text-center p-4 rounded-full border-2 border-black hover:border-[#00ff00] outline-none "
+              className="w-[90%] text-lg text-center p-4 rounded-full border-2 border-black hover:border-[#00ff00] outline-none "
               type="text"
               name="username"
               // required
-              placeholder="enter a username"
+              placeholder="User Name"
             />
           </div>
           <div className="flex flex-col items-center gap-3   rounded-full   p-1">
             {/* <label htmlFor="username">User Name</label> */}
             <input
-              className="w-[90%] text-center p-4 rounded-full border-2 border-black hover:border-[#00ff00] outline-none "
+              className="w-[90%] text-lg text-center p-4 rounded-full border-2 border-black hover:border-[#00ff00] outline-none "
               // required
               type="password"
               name="password"
-              placeholder="enter a password"
+              placeholder="Password"
             />
           </div>
           <div className="flex flex-row items-center justify-center gap-3   rounded-full  p-1">
             <input className=" " type="checkbox" id="scales" name="scales" />
             <label>
               I Accept the{" "}
-              <span className="text-[#00fa00ff] ">Privacy Policy</span>
+              <span className="text-[#00af00ff] font-bold">Privacy Policy</span>
             </label>
           </div>
+
           <div className="flex flex-col items-center gap-3   rounded-full   p-1">
             <button
               className="relative w-[90%] text-white text-lg font-bold uppercase text-center p-4 rounded-full create_account_button"
@@ -73,6 +75,16 @@ const RegisterPage = () => {
               <span className="absolute bottom-0 right-5 w-[70px] h-[20px] rounded-l-md bg-[#ffffff23] "></span>
               <span className="absolute top-0 right-0 w-[20px] h-[60px] rounded-l-lg  bg-[#ffffff23] "></span>
             </button>
+          </div>
+          {/*  */}
+          <div className="flex flex-row items-center justify-center gap-3   rounded-full  p-1">
+            {/* <input className=" " type="checkbox" id="scales" name="scales" /> */}
+            <label>
+              Already Have an Account?{" "}
+              <span className="text-[#00af00ff] font-bold">
+                <Link to={"/auth/login"}>Log In</Link>
+              </span>
+            </label>
           </div>
         </form>
       </section>
